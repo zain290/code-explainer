11fb589b4ce46d4e31b493e4f3735af1784838b3
zain290 <zain290@users.noreply.github.com>
2026-07-14 20:55:18 +0500

    Revert subpath config, switch to subdomain routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Contact } from './pages/Contact'
 
 export function App() {
   return (
-    <BrowserRouter basename="/codex">
+    <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
